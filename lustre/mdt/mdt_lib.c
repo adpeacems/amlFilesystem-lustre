lustre/lustre-release

commit 4c2514f4832801374092f3a48c755248af345566

LU-14459 mdt: support fixed directory layout
User may not want directories split automatically in some cases:
*.directory migrated.
* directory restriped.

To support this, an LMV flag LMV_HASH_FLAG_FIXED is added, and it will
be set on migrated/restriped directories. NB, if directory is migrated
or restriped to a one-stripe directory, it won't be transformed into a
plain directory, because this flag needs to be kept.

Update sanity 230q.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Icd12b2aa34d391e32c3323a8b9c24449ea3e3d0e
Reviewed-on: https://review.whamcloud.com/43291
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_lib.c
@@ -1559,8 +1559,12 @@ static int mdt_migrate_unpack(struct mdt_thread_info *info)
 								RCL_CLIENT);
 
 			if (rr->rr_eadatalen > 0) {
-				rr->rr_eadata = req_capsule_client_get(pill,
-								&RMF_EADATA);
+				struct lmv_user_md_v1 *lmu;
+
+				lmu = req_capsule_client_get(pill, &RMF_EADATA);
+				lmu->lum_hash_type |=
+					cpu_to_le32(LMV_HASH_FLAG_FIXED);
+				rr->rr_eadata = lmu;
 				spec->u.sp_ea.eadatalen = rr->rr_eadatalen;
 				spec->u.sp_ea.eadata = rr->rr_eadata;
 				spec->sp_cr_flags |= MDS_OPEN_HAS_EA;

lustre/include/lustre_lmv.h
@@ -504,4 +504,9 @@ static inline bool lmv_is_layout_changing(const struct lmv_mds_md_v1 *lmv)
 	       lmv_hash_is_migrating(cpu_to_le32(lmv->lmv_hash_type));
 }
 
+static inline bool lmv_is_fixed(const struct lmv_mds_md_v1 *lmv)
+{
+	return cpu_to_le32(lmv->lmv_hash_type) & LMV_HASH_FLAG_FIXED;
+}
+
 #endif

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1020,6 +1020,9 @@ static inline bool lmv_is_known_hash_type(__u32 type)
 	       (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_CRUSH;
 }
 
+/* fixed layout, such directories won't split automatically */
+/* NB, update LMV_HASH_FLAG_KNOWN when adding new flag */
+#define LMV_HASH_FLAG_FIXED		0x02000000
 #define LMV_HASH_FLAG_MERGE		0x04000000
 #define LMV_HASH_FLAG_SPLIT		0x08000000
 
@@ -1033,6 +1036,8 @@ static inline bool lmv_is_known_hash_type(__u32 type)
 
 #define LMV_HASH_FLAG_LAYOUT_CHANGE	\
 	(LMV_HASH_FLAG_MIGRATION | LMV_HASH_FLAG_SPLIT | LMV_HASH_FLAG_MERGE)
+
+#define LMV_HASH_FLAG_KNOWN		0xfe000000
 
 /* both SPLIT and MIGRATION are set for directory split */
 static inline bool lmv_hash_is_splitting(__u32 hash)

lustre/lod/lod_object.c
@@ -2413,9 +2413,11 @@ static int lod_dir_layout_set(const struct lu_env *env,
 		RETURN(-EINVAL);
 
 	/* adjust hash for dir merge, which may not be set in user command */
-	if (lmv_is_merging(lmv) && !lmv->lmv_migrate_hash)
-		lmv->lmv_merge_hash =
-			lod->lod_mdt_descs.ltd_lmv_desc.ld_pattern;
+	if (lmv_is_merging(lmv) &&
+	    !(lmv->lmv_migrate_hash & LMV_HASH_TYPE_MASK))
+		lmv->lmv_merge_hash |=
+			lod->lod_mdt_descs.ltd_lmv_desc.ld_pattern &
+			LMV_HASH_TYPE_MASK;
 
 	LMV_DEBUG(D_INFO, lmv, "set");
 
@@ -8266,9 +8268,9 @@ static int lod_dir_declare_layout_shrink(const struct lu_env *env,
 	struct lod_object *lo = lod_dt_obj(dt);
 	struct dt_object *next = dt_object_child(dt);
 	struct lmv_user_md *lmu = mlc->mlc_buf.lb_buf;
-	__u32 final_stripe_count;
 	char *stripe_name = info->lti_key;
 	struct lu_buf *lmv_buf = &info->lti_buf;
+	__u32 final_stripe_count;
 	struct dt_object *dto;
 	int i;
 	int rc;
@@ -8294,9 +8296,6 @@ static int lod_dir_declare_layout_shrink(const struct lu_env *env,
 			continue;
 
 		if (i < final_stripe_count) {
-			if (final_stripe_count == 1)
-				continue;
-
 			rc = lod_sub_declare_xattr_set(env, dto, lmv_buf,
 						       XATTR_NAME_LMV,
 						       LU_XATTR_REPLACE, th);
@@ -8520,7 +8519,8 @@ static int lod_dir_layout_shrink(const struct lu_env *env,
 	lmv->lmv_magic = cpu_to_le32(LMV_MAGIC_STRIPE);
 	lmv->lmv_stripe_count = cpu_to_le32(final_stripe_count);
 	lmv->lmv_hash_type = cpu_to_le32(lo->ldo_dir_hash_type) &
-			     cpu_to_le32(LMV_HASH_TYPE_MASK);
+			     cpu_to_le32(LMV_HASH_TYPE_MASK |
+					 LMV_HASH_FLAG_FIXED);
 	lmv->lmv_layout_version =
 			cpu_to_le32(lo->ldo_dir_layout_version + 1);
 	lmv->lmv_migrate_offset = 0;
@@ -8532,14 +8532,6 @@ static int lod_dir_layout_shrink(const struct lu_env *env,
 			continue;
 
 		if (i < final_stripe_count) {
-			/* if only one stripe left, no need to update
-			 * LMV because this stripe will replace master
-			 * object and act as a plain directory.
-			 */
-			if (final_stripe_count == 1)
-				continue;
-
-
 			rc = lod_fld_lookup(env, lod,
 					    lu_object_fid(&dto->do_lu),
 					    &mdtidx, &type);

lustre/mdd/mdd_dir.c
@@ -4100,7 +4100,10 @@ static int mdd_migrate_cmd_check(struct mdd_device *mdd,
 				 const struct lu_name *lname)
 {
 	__u32 lum_stripe_count = lum->lum_stripe_count;
-	__u32 lmv_hash_type = lmv->lmv_hash_type;
+	__u32 lum_hash_type = lum->lum_hash_type &
+			      cpu_to_le32(LMV_HASH_TYPE_MASK);
+	__u32 lmv_hash_type = lmv->lmv_hash_type &
+			      cpu_to_le32(LMV_HASH_TYPE_MASK);
 
 	if (!lmv_is_sane(lmv))
 		return -EBADF;
@@ -4109,12 +4112,10 @@ static int mdd_migrate_cmd_check(struct mdd_device *mdd,
 	if (!lum_stripe_count)
 		lum_stripe_count = cpu_to_le32(1);
 
-	lmv_hash_type &= ~cpu_to_le32(LMV_HASH_FLAG_MIGRATION);
-
 	/* TODO: check specific MDTs */
 	if (lum_stripe_count != lmv->lmv_migrate_offset ||
 	    lum->lum_stripe_offset != lmv->lmv_master_mdt_index ||
-	    (lum->lum_hash_type && lum->lum_hash_type != lmv_hash_type)) {
+	    (lum_hash_type && lum_hash_type != lmv_hash_type)) {
 		CERROR("%s: '"DNAME"' migration was interrupted, run 'lfs migrate -m %d -c %d -H %s "DNAME"' to finish migration.\n",
 			mdd2obd_dev(mdd)->obd_name, PNAME(lname),
 			le32_to_cpu(lmv->lmv_master_mdt_index),
@@ -4647,7 +4648,7 @@ int mdd_dir_layout_shrink(const struct lu_env *env,
 	if (rc)
 		GOTO(stop_trans, rc);
 
-	if (le32_to_cpu(lmu->lum_stripe_count) == 1) {
+	if (le32_to_cpu(lmu->lum_stripe_count) == 1 && !lmv_is_fixed(lmv)) {
 		rc = mdd_declare_1sd_collapse(env, pobj, obj, stripe, attr, mlc,
 					      &lname, handle);
 		if (rc)
@@ -4670,7 +4671,7 @@ int mdd_dir_layout_shrink(const struct lu_env *env,
 	if (rc)
 		GOTO(stop_trans, rc);
 
-	if (le32_to_cpu(lmu->lum_stripe_count) == 1) {
+	if (le32_to_cpu(lmu->lum_stripe_count) == 1 && !lmv_is_fixed(lmv)) {
 		rc = mdd_1sd_collapse(env, pobj, obj, stripe, attr, mlc, &lname,
 				      handle);
 		if (rc)

lustre/mdt/mdt_handler.c
@@ -1419,6 +1419,7 @@ static int mdt_getattr_internal(struct mdt_thread_info *info,
 
 	if (mdt_body_has_lov(la, reqbody)) {
 		u32 stripe_count = 1;
+		bool fixed_layout = false;
 
 		if (ma->ma_valid & MA_LOV) {
 			LASSERT(ma->ma_lmm_size);
@@ -1443,6 +1444,7 @@ static int mdt_getattr_internal(struct mdt_thread_info *info,
 			repbody->mbo_valid |= (OBD_MD_FLDIREA|OBD_MD_MEA);
 
 			stripe_count = le32_to_cpu(lmv->lmv_stripe_count);
+			fixed_layout = lmv_is_fixed(lmv);
 			if (magic == LMV_MAGIC_STRIPE && lmv_is_restriping(lmv))
 				mdt_restripe_migrate_add(info, o);
 			else if (magic == LMV_MAGIC_V1 &&
@@ -1474,7 +1476,8 @@ static int mdt_getattr_internal(struct mdt_thread_info *info,
 		    !fid_is_root(mdt_object_fid(o)) &&
 		    mdt->mdt_enable_dir_auto_split &&
 		    !o->mot_restriping &&
-		    stripe_count < atomic_read(&mdt->mdt_mds_mds_conns) + 1)
+		    stripe_count < atomic_read(&mdt->mdt_mds_mds_conns) + 1 &&
+		    !fixed_layout)
 			mdt_auto_split_add(info, o);
 	} else if (S_ISLNK(la->la_mode) &&
 		   reqbody->mbo_valid & OBD_MD_LINKNAME) {

lustre/mdt/mdt_reint.c
@@ -352,6 +352,7 @@ static int mdt_restripe(struct mdt_thread_info *info,
 	struct mdt_device *mdt = info->mti_mdt;
 	struct lu_fid *fid = &info->mti_tmp_fid2;
 	struct ldlm_enqueue_info *einfo = &info->mti_einfo[0];
+	struct lmv_user_md *lum = spec->u.sp_ea.eadata;
 	struct lmv_mds_md_v1 *lmv;
 	struct mdt_object *child;
 	struct mdt_lock_handle *lhp;
@@ -362,6 +363,9 @@ static int mdt_restripe(struct mdt_thread_info *info,
 	ENTRY;
 	if (!mdt->mdt_enable_dir_restripe)
 		RETURN(-EPERM);
+
+	LASSERT(lum);
+	lum->lum_hash_type |= cpu_to_le32(LMV_HASH_FLAG_FIXED);
 
 	rc = mdt_version_get_check_save(info, parent, 0);
 	if (rc)

lustre/mdt/mdt_restripe.c
@@ -244,6 +244,8 @@ int mdt_restripe_internal(struct mdt_thread_info *info,
 
 		lmv->lmv_hash_type |= cpu_to_le32(LMV_HASH_FLAG_MERGE |
 						  LMV_HASH_FLAG_MIGRATION);
+		lmv->lmv_hash_type |= lum->lum_hash_type &
+				      cpu_to_le32(LMV_HASH_FLAG_FIXED);
 		lmv->lmv_merge_offset = lum->lum_stripe_count;
 		lmv->lmv_merge_hash = lum->lum_hash_type;
 		lmv->lmv_layout_version = cpu_to_le32(++version);
@@ -605,7 +607,8 @@ static int mdt_restripe_migrate(struct mdt_thread_info *info)
 	if ((lmv_is_splitting(lmv) &&
 	     idx >= le32_to_cpu(lmv->lmv_split_offset)) ||
 	    (lmv_is_merging(lmv) &&
-	     le32_to_cpu(lmv->lmv_hash_type) == LMV_HASH_TYPE_CRUSH &&
+	     (le32_to_cpu(lmv->lmv_hash_type) & LMV_HASH_TYPE_MASK) ==
+		LMV_HASH_TYPE_CRUSH &&
 	     idx < le32_to_cpu(lmv->lmv_merge_offset))) {
 		/* new stripes doesn't need to migrate sub files in dir
 		 * split, neither for target stripes in dir merge if hash type

adpeacems/amlFilesystem-lustre

commit 1a7759fbaa182a188ecf6921f4c046667e0000e2

LU-17810 dne: dir restripe without fixed hash flag
Add an option to clear LMV_HASH_FIXED_FLAG
in dir re-striping, this restores an ability to convert a
striped dir with only one stripe into a plain dir.

HPE-bug-id: LUS-12321
Fixes: 4c2514f483 ("LU-14459 mdt: support fixed directory layout")
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: I7c119f435b19a446cb960cdc5a8ebec7e00ab0dc

lustre/doc/lfs-migrate.1
@@ -149,6 +149,10 @@ Only migrate the specified \fIDIRECTORY\fR and the non-directory inodes that are
 directly located within it.
 Similar to '\fBls -d\fR' and '\fBlfs getstripe -d\fR'.
 .TP
+.BR --clear-fixed
+Do not set LMV_HASH_FIXED_FLAG upon migrating the directory. It allows a striped dir
+with only one stripe to be converted to a plain directory.
+.TP
 .BR -H , --mdt-hash=\fIHASH_TYPE\fR
 Use
 .I HASH_TYPE

lustre/mdt/mdt_lib.c
@@ -1720,8 +1720,6 @@ static int mdt_migrate_unpack(struct mdt_thread_info *info)
 				struct lmv_user_md_v1 *lmu;
 
 				lmu = req_capsule_client_get(pill, &RMF_EADATA);
-				lmu->lum_hash_type |=
-					cpu_to_le32(LMV_HASH_FLAG_FIXED);
 				rr->rr_eadata = lmu;
 				spec->u.sp_ea.eadatalen = rr->rr_eadatalen;
 				spec->u.sp_ea.eadata = rr->rr_eadata;

lustre/utils/lfs.c
@@ -3630,6 +3630,7 @@ enum {
 	LFS_LINKS_OPT,
 	LFS_ATTRS_OPT,
 	LFS_XATTRS_MATCH_OPT,
+	LFS_MIGRATE_NOFIX,
 };
 
 #ifndef LCME_USER_MIRROR_FLAGS
@@ -3681,6 +3682,7 @@ static int lfs_setstripe_internal(int argc, char **argv,
 	bool foreign_mode = false;
 	char *xattr = NULL;
 	bool overstriped = false;
+	bool clear_hash_fixed = false;
 	uint32_t type = LU_FOREIGN_TYPE_NONE, flags = 0;
 	char *mode_opt = NULL;
 	mode_t previous_umask = 0;
@@ -3787,6 +3789,7 @@ static int lfs_setstripe_internal(int argc, char **argv,
 	{ .val = 'y',	.name = "yaml",		.has_arg = required_argument },
 	{ .val = 'z',   .name = "ext-size",	.has_arg = required_argument},
 	{ .val = 'z',   .name = "extension-size", .has_arg = required_argument},
+	{ .val = LFS_MIGRATE_NOFIX, .name = "clear-fixed", .has_arg = no_argument},
 	{ .name = NULL } };
 
 	setstripe_args_init(&lsa);
@@ -3951,6 +3954,15 @@ static int lfs_setstripe_internal(int argc, char **argv,
 					progname, argv[0], optarg);
 				goto usage_error;
 			}
+			break;
+		case LFS_MIGRATE_NOFIX:
+			if (!migrate_mode) {
+				fprintf(stderr,
+					"%s %s: --clear-fixed valid only for migrate command\n",
+					progname, argv[0]);
+				goto usage_error;
+			}
+			clear_hash_fixed = true;
 			break;
 		case 'b':
 			if (!migrate_mode) {
@@ -4558,6 +4570,9 @@ create_mirror:
 
 		if (overstriped)
 			lmu->lum_hash_type |= LMV_HASH_FLAG_OVERSTRIPED;
+
+		if (!clear_hash_fixed)
+			lmu->lum_hash_type |= LMV_HASH_FLAG_FIXED;
 
 		if (lsa.lsa_pool_name)
 			snprintf(lmu->lum_pool_name, sizeof(lmu->lum_pool_name),