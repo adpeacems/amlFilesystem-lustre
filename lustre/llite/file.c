lustre/lustre-release

commit 3177b0dc5d18a8e3d77eb10fa6f266ff83cbe222

LU-2525 ldlm: add asynchronous flocks
Add support of asynchronous flocks.
They are used only by Linux nfsd for now.

HPE-bug-id: LUS-3210, LUS-7034,LUS-7031,LUS-8832, LUS-8313
HPE-bug-id: LUS-8592
Change-Id: Iefafaf014fd06d569dc5d1dd22ebb3518d04e99a
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Signed-off-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/4889
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/file.c
@@ -5244,44 +5244,25 @@ int ll_fsync(struct file *file, loff_t start, loff_t end, int datasync)
 	RETURN(rc);
 }
 
-static int
-ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
+static int ll_file_flc2policy(struct file_lock *file_lock, int cmd,
+		       union ldlm_policy_data *flock)
 {
-	struct inode *inode = file_inode(file);
-	struct ll_sb_info *sbi = ll_i2sbi(inode);
-	struct ldlm_enqueue_info einfo = {
-		.ei_type	= LDLM_FLOCK,
-		.ei_cb_cp	= ldlm_flock_completion_ast,
-		.ei_cbdata	= file_lock,
-	};
-	struct md_op_data *op_data;
-	struct lustre_handle lockh = { 0 };
-	union ldlm_policy_data flock = { { 0 } };
-	struct file_lock flbuf = *file_lock;
-	int fl_type = file_lock->C_FLC_TYPE;
-	ktime_t kstart = ktime_get();
-	__u64 flags = 0;
-	int rc;
-	int rc2 = 0;
-
 	ENTRY;
-	CDEBUG(D_VFSTRACE, "VFS Op:inode="DFID" file_lock=%p\n",
-	       PFID(ll_inode2fid(inode)), file_lock);
 
 	if (file_lock->C_FLC_FLAGS & FL_FLOCK) {
 		LASSERT((cmd == F_SETLKW) || (cmd == F_SETLK));
 		/* flocks are whole-file locks */
-		flock.l_flock.end = OFFSET_MAX;
+		flock->l_flock.end = OFFSET_MAX;
 		/* For flocks owner is determined by the local file desctiptor*/
-		flock.l_flock.owner = (unsigned long)file_lock->C_FLC_FILE;
+		flock->l_flock.owner = (unsigned long)file_lock->C_FLC_FILE;
 	} else if (file_lock->C_FLC_FLAGS & FL_POSIX) {
-		flock.l_flock.owner = (unsigned long)file_lock->C_FLC_OWNER;
-		flock.l_flock.start = file_lock->fl_start;
-		flock.l_flock.end = file_lock->fl_end;
+		flock->l_flock.owner = (unsigned long)file_lock->C_FLC_OWNER;
+		flock->l_flock.start = file_lock->fl_start;
+		flock->l_flock.end = file_lock->fl_end;
 	} else {
 		RETURN(-EINVAL);
 	}
-	flock.l_flock.pid = file_lock->C_FLC_PID;
+	flock->l_flock.pid = file_lock->C_FLC_PID;
 
 #if defined(HAVE_LM_COMPARE_OWNER) || defined(lm_compare_owner)
 	/* Somewhat ugly workaround for svc lockd.
@@ -5293,8 +5274,208 @@ ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
 	 * pointer space for current->files are not intersecting
 	 */
 	if (file_lock->fl_lmops && file_lock->fl_lmops->lm_compare_owner)
-		flock.l_flock.owner = (unsigned long)file_lock->C_FLC_PID;
+		flock->l_flock.owner = (unsigned long)file_lock->C_FLC_PID;
 #endif
+
+	RETURN(0);
+}
+
+static int ll_file_flock_lock(struct file *file, struct file_lock *file_lock)
+{
+	int rc = -EINVAL;
+
+	/* We don't need to sleep on conflicting locks.
+	 * It is called in following usecases :
+	 * 1. adding new lock - no conflicts exist as it is already granted
+	 *    on the server.
+	 * 2. unlock - never conflicts with anything.
+	 */
+	file_lock->fl_flags &= ~FL_SLEEP;
+#ifdef HAVE_LOCKS_LOCK_FILE_WAIT
+	rc = locks_lock_file_wait(file, file_lock);
+#else
+	if (file_lock->fl_flags & FL_FLOCK) {
+		rc = flock_lock_file_wait(file, file_lock);
+	} else if (file_lock->fl_flags & FL_POSIX) {
+		rc = posix_lock_file(file, file_lock, NULL);
+	}
+#endif /* HAVE_LOCKS_LOCK_FILE_WAIT */
+	if (rc)
+		CDEBUG_LIMIT(rc == -ENOENT ? D_DLMTRACE : D_ERROR,
+		       "kernel lock failed: rc = %d\n", rc);
+
+	return rc;
+}
+
+static int ll_flock_upcall(void *cookie, int err);
+static int
+ll_flock_completion_ast_async(struct ldlm_lock *lock, __u64 flags, void *data);
+
+static int ll_file_flock_async_unlock(struct inode *inode,
+				      struct file_lock *file_lock)
+{
+	struct ll_sb_info *sbi = ll_i2sbi(inode);
+	struct ldlm_enqueue_info einfo = { .ei_type = LDLM_FLOCK,
+					   .ei_cb_cp =
+					      ll_flock_completion_ast_async,
+					   .ei_mode = LCK_NL,
+					   .ei_cbdata = NULL };
+	union ldlm_policy_data flock = { {0} };
+	struct md_op_data *op_data;
+	int rc;
+
+	ENTRY;
+	rc = ll_file_flc2policy(file_lock, F_SETLK, &flock);
+	if (rc)
+		RETURN(rc);
+
+	op_data = ll_prep_md_op_data(NULL, inode, NULL, NULL, 0, 0,
+				     LUSTRE_OPC_ANY, NULL);
+	if (IS_ERR(op_data))
+		RETURN(PTR_ERR(op_data));
+
+	rc = md_enqueue_async(sbi->ll_md_exp, &einfo, ll_flock_upcall,
+			      op_data, &flock, 0);
+
+	ll_finish_md_op_data(op_data);
+
+	RETURN(rc);
+}
+
+/* This function is called only once after ldlm callback. Args are already
+ * detached from lock. So, locking isn't needed.
+ * It should only report lock status to kernel.
+ */
+static void ll_file_flock_async_cb(struct ldlm_flock_info *args)
+{
+	struct file_lock *file_lock = args->fa_fl;
+	struct file_lock *flc = &args->fa_flc;
+	struct file *file = args->fa_file;
+	struct inode *inode = file->f_path.dentry->d_inode;
+	int err = args->fa_err;
+	int rc;
+
+	ENTRY;
+	CDEBUG(D_INFO, "err=%d file_lock=%p file=%p start=%llu end=%llu\n",
+	       err, file_lock, file, flc->fl_start, flc->fl_end);
+
+	/* The kernel is responsible for resolving grant vs F_CANCELK or
+	 * grant vs. cleanup races, it may happen that CANCELED flag
+	 * isn't set and err == 0, because f_CANCELK/cleanup happens between
+	 * ldlm_flock_completion_ast_async() and ll_flock_run_flock_cb().
+	 * In this case notify() returns error for already canceled flock.
+	 */
+	if (!(args->fa_flags & FA_FL_CANCELED)) {
+		struct file_lock notify_lock;
+
+		locks_init_lock(&notify_lock);
+		locks_copy_lock(&notify_lock, flc);
+
+		if (err == 0)
+			ll_file_flock_lock(file, flc);
+
+		wait_event_idle(args->fa_waitq, args->fa_ready);
+
+#ifdef HAVE_LM_GRANT_2ARGS
+		rc = args->fa_notify(&notify_lock, err);
+#else
+		rc = args->fa_notify(&notify_lock, NULL, err);
+#endif
+		if (rc) {
+			CDEBUG_LIMIT(D_ERROR,
+				     "notify failed file_lock=%p err=%d\n",
+				     file_lock, err);
+			if (err == 0) {
+				flc->C_FLC_TYPE = F_UNLCK;
+				ll_file_flock_lock(file, flc);
+				ll_file_flock_async_unlock(inode, flc);
+			}
+		}
+	}
+
+	fput(file);
+
+	EXIT;
+}
+
+static void ll_flock_run_flock_cb(struct ldlm_flock_info *args)
+{
+	if (args) {
+		ll_file_flock_async_cb(args);
+		OBD_FREE_PTR(args);
+	}
+}
+
+static int ll_flock_upcall(void *cookie, int err)
+{
+	struct ldlm_flock_info *args;
+	struct ldlm_lock *lock = cookie;
+
+	if (err != 0) {
+		CERROR("ldlm_cli_enqueue_fini lock=%p : rc = %d\n", lock, err);
+
+		lock_res_and_lock(lock);
+		args = lock->l_ast_data;
+		lock->l_ast_data = NULL;
+		unlock_res_and_lock(lock);
+
+		if (args)
+			args->fa_err = err;
+		ll_flock_run_flock_cb(args);
+	}
+
+	return 0;
+}
+
+static int
+ll_flock_completion_ast_async(struct ldlm_lock *lock, __u64 flags, void *data)
+{
+	struct ldlm_flock_info *args;
+
+	ENTRY;
+
+	args = ldlm_flock_completion_ast_async(lock, flags, data);
+	if (args && args->fa_flags & FA_FL_CANCELED) {
+		/* lock was cancelled in a race */
+		struct inode *inode = args->fa_file->f_path.dentry->d_inode;
+
+		ll_file_flock_async_unlock(inode, &args->fa_flc);
+	}
+
+	ll_flock_run_flock_cb(args);
+
+	RETURN(0);
+}
+
+static int
+ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
+{
+	struct inode *inode = file_inode(file);
+	struct ll_sb_info *sbi = ll_i2sbi(inode);
+	struct ldlm_enqueue_info einfo = {
+		.ei_type	= LDLM_FLOCK,
+		.ei_cb_cp	= ldlm_flock_completion_ast,
+		.ei_cbdata	= NULL,
+	};
+	struct md_op_data *op_data;
+	struct lustre_handle lockh = { 0 };
+	union ldlm_policy_data flock = { { 0 } };
+	struct file_lock flbuf = *file_lock;
+	int fl_type = file_lock->C_FLC_TYPE;
+	ktime_t kstart = ktime_get();
+	__u64 flags = 0;
+	struct ldlm_flock_info *cb_data = NULL;
+	int rc;
+
+	ENTRY;
+	CDEBUG(D_VFSTRACE, "VFS Op:inode="DFID" file_lock=%p\n",
+	       PFID(ll_inode2fid(inode)), file_lock);
+
+	ll_stats_ops_tally(ll_i2sbi(inode), LPROC_LL_FLOCK, 1);
+
+	rc = ll_file_flc2policy(file_lock, cmd, &flock);
+	if (rc)
+		RETURN(rc);
 
 	switch (fl_type) {
 	case F_RDLCK:
@@ -5346,6 +5527,13 @@ ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
 		 */
 		posix_test_lock(file, &flbuf);
 		break;
+	case F_CANCELLK:
+		CDEBUG(D_DLMTRACE, "F_CANCELLK owner=%llx %llu-%llu\n",
+		       flock.l_flock.owner, flock.l_flock.start,
+		       flock.l_flock.end);
+		file_lock->C_FLC_TYPE = F_UNLCK;
+		einfo.ei_mode = LCK_NL;
+		break;
 	default:
 		rc = -EINVAL;
 		CERROR("%s: fcntl from '%s' unknown lock command=%d: rc = %d\n",
@@ -5353,51 +5541,102 @@ ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
 		RETURN(rc);
 	}
 
-	/* Save the old mode so that if the mode in the lock changes we
-	 * can decrement the appropriate reader or writer refcount.
-	 */
-	file_lock->C_FLC_TYPE = einfo.ei_mode;
+	CDEBUG(D_DLMTRACE,
+	       "inode="DFID", pid=%u, owner=%#llx, flags=%#llx, mode=%u, start=%llu, end=%llu\n",
+	       PFID(ll_inode2fid(inode)), flock.l_flock.pid,
+	       flock.l_flock.owner, flags, einfo.ei_mode,
+	       flock.l_flock.start, flock.l_flock.end);
 
 	op_data = ll_prep_md_op_data(NULL, inode, NULL, NULL, 0, 0,
 				     LUSTRE_OPC_ANY, NULL);
 	if (IS_ERR(op_data))
 		RETURN(PTR_ERR(op_data));
 
-	CDEBUG(D_DLMTRACE,
-	       "inode="DFID", pid=%u, flags=%#llx, mode=%u, start=%llu, end=%llu\n",
-	       PFID(ll_inode2fid(inode)),
-	       flock.l_flock.pid, flags, einfo.ei_mode,
-	       flock.l_flock.start, flock.l_flock.end);
+	OBD_ALLOC_PTR(cb_data);
+	if (!cb_data)
+		GOTO(out, rc = -ENOMEM);
 
-	rc = md_enqueue(sbi->ll_md_exp, &einfo, &flock, op_data, &lockh,
-			flags);
+	cb_data->fa_file = file;
+	cb_data->fa_fl = file_lock;
+	cb_data->fa_mode = einfo.ei_mode;
+	init_waitqueue_head(&cb_data->fa_waitq);
+	locks_init_lock(&cb_data->fa_flc);
+	locks_copy_lock(&cb_data->fa_flc, file_lock);
+	if (cmd == F_CANCELLK)
+		cb_data->fa_flags |= FA_FL_CANCEL_RQST;
+	einfo.ei_cbdata = cb_data;
 
-	/* Restore the file lock type if not TEST lock. */
-	if (!(flags & LDLM_FL_TEST_LOCK))
-		file_lock->C_FLC_TYPE = fl_type;
+	if (file_lock->fl_lmops && file_lock->fl_lmops->lm_grant &&
+	    file_lock->C_FLC_TYPE != F_UNLCK &&
+	    flags == LDLM_FL_BLOCK_NOWAIT /* F_SETLK/F_SETLK64 */) {
 
-#ifdef HAVE_LOCKS_LOCK_FILE_WAIT
-	if ((rc == 0 || file_lock->C_FLC_TYPE == F_UNLCK) &&
-	    !(flags & LDLM_FL_TEST_LOCK))
-		rc2  = locks_lock_file_wait(file, file_lock);
-#else
-	if ((file_lock->C_FLC_FLAGS & FL_FLOCK) &&
-	    (rc == 0 || file_lock->C_FLC_TYPE == F_UNLCK))
-		rc2  = flock_lock_file_wait(file, file_lock);
-	if ((file_lock->C_FLC_FLAGS & FL_POSIX) &&
-	    (rc == 0 || file_lock->C_FLC_TYPE == F_UNLCK) &&
-	    !(flags & LDLM_FL_TEST_LOCK))
-		rc2  = posix_lock_file_wait(file, file_lock);
-#endif /* HAVE_LOCKS_LOCK_FILE_WAIT */
+		cb_data->fa_notify = file_lock->fl_lmops->lm_grant;
+		flags = (file_lock->fl_flags & FL_SLEEP) ?
+			0 : LDLM_FL_BLOCK_NOWAIT;
+		einfo.ei_cb_cp = ll_flock_completion_ast_async;
+		get_file(file);
 
-	if (rc2 && file_lock->C_FLC_TYPE != F_UNLCK) {
-		einfo.ei_mode = LCK_NL;
-		md_enqueue(sbi->ll_md_exp, &einfo, &flock, op_data,
-			   &lockh, flags);
-		rc = rc2;
+		rc = md_enqueue_async(sbi->ll_md_exp, &einfo,
+				      ll_flock_upcall, op_data, &flock, flags);
+		if (rc) {
+			fput(file);
+			OBD_FREE_PTR(cb_data);
+			cb_data = NULL;
+		} else {
+			rc = FILE_LOCK_DEFERRED;
+		}
+	} else {
+		if (file_lock->C_FLC_TYPE == F_UNLCK &&
+		    flags != LDLM_FL_TEST_LOCK) {
+			/* We unlock kernel lock before ldlm one to avoid race
+			 * with reordering of unlock & lock responses from
+			 * server.
+			 */
+			cb_data->fa_flc.fl_flags |= FL_EXISTS;
+			rc = ll_file_flock_lock(file, &cb_data->fa_flc);
+			if (rc) {
+				if (rc == -ENOENT) {
+					if (!(file_lock->C_FLC_TYPE &
+								FL_EXISTS))
+						rc = 0;
+				} else {
+					CDEBUG_LIMIT(D_ERROR,
+					       "local unlock failed rc=%d\n",
+					       rc);
+				}
+				OBD_FREE_PTR(cb_data);
+				cb_data = NULL;
+				GOTO(out, rc);
+			}
+		}
+
+		rc = md_enqueue(sbi->ll_md_exp, &einfo, &flock, op_data,
+				&lockh, flags);
+
+
+		if (!rc && file_lock->C_FLC_TYPE != F_UNLCK &&
+		    !(flags & LDLM_FL_TEST_LOCK)) {
+			int rc2;
+
+			rc2 = ll_file_flock_lock(file, file_lock);
+
+			if (rc2) {
+				einfo.ei_mode = LCK_NL;
+				cb_data->fa_mode = einfo.ei_mode;
+				md_enqueue(sbi->ll_md_exp, &einfo, &flock,
+					   op_data, &lockh, flags);
+				rc = rc2;
+			}
+		}
+		OBD_FREE_PTR(cb_data);
+		cb_data = NULL;
 	}
-
+out:
 	ll_finish_md_op_data(op_data);
+	if (cb_data) {
+		cb_data->fa_ready = 1;
+		wake_up(&cb_data->fa_waitq);
+	}
 
 	if (rc == 0 && (flags & LDLM_FL_TEST_LOCK) &&
 	    flbuf.C_FLC_TYPE != file_lock->C_FLC_TYPE) { /* Verify local & remote */

lustre/autoconf/lustre-core.m4
@@ -1146,6 +1146,23 @@ AC_DEFUN([LC_HAVE_BLK_INTEGRITY_ITER], [
 ]) # LC_HAVE_BLK_INTEGRITY_ITER
 
 #
+# LC_HAVE_LM_GRANT_2ARGS
+#
+# 3.17 removed unused argument from lm_grant
+#
+AC_DEFUN([LC_HAVE_LM_GRANT_2ARGS], [
+LB_CHECK_COMPILE([if 'lock_manager_operations.lm_grant' takes two args],
+lm_grant, [
+	#include <linux/fs.h>
+],[
+	((struct lock_manager_operations *)NULL)->lm_grant(NULL, 0);
+],[
+	AC_DEFINE(HAVE_LM_GRANT_2ARGS, 1,
+		[lock_manager_operations.lm_grant takes two args])
+])
+]) # LC_HAVE_LM_GRANT_2ARGS
+
+#
 # LC_NFS_FILLDIR_USE_CTX
 #
 # 3.18 kernel moved from void cookie to struct dir_context
@@ -4255,6 +4272,8 @@ AC_DEFUN([LC_HAVE_LOCKS_LOCK_FILE_WAIT_IN_FILELOCK], [
 			[kernel has locks_lock_file_wait in filelock.h])
 		AC_DEFINE(HAVE_LINUX_FILELOCK_HEADER, 1,
 			[linux/filelock.h is present])
+		AC_DEFINE(HAVE_LM_GRANT_2ARGS, 1,
+			[lock_manager_operations.lm_grant takes two args])
 	])
 ]) # LC_HAVE_LOCKS_LOCK_FILE_WAIT_IN_FILELOCK
 
@@ -5079,6 +5098,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_INTERVAL_BLK_INTEGRITY
 	LC_KEY_MATCH_DATA
 	LC_HAVE_BLK_INTEGRITY_ITER
+	LC_HAVE_LM_GRANT_2ARGS
 
 	# 3.18
 	LC_PERCPU_COUNTER_INIT

lustre/include/lustre_dlm.h
@@ -30,6 +30,9 @@
 #include <lustre_import.h>
 #include <lustre_handles.h>
 #include <linux/interval_tree_generic.h>
+#ifdef HAVE_LINUX_FILELOCK_HEADER
+#include <linux/filelock.h>
+#endif
 
 #include "lustre_dlm_flags.h"
 
@@ -1113,6 +1116,8 @@ struct ldlm_resource {
 	 * that are waiting for conflicts to go away
 	 */
 	struct list_head	lr_waiting;
+	/* List of locks that waiting to enqueueing for flock */
+	struct list_head	lr_enqueueing;
 	/** @} */
 
 	/** Resource name */
@@ -1287,6 +1292,27 @@ struct ldlm_enqueue_info {
 
 #define ei_res_id	ei_cb_gl
 
+enum ldlm_flock_flags {
+	FA_FL_CANCEL_RQST	= 1,
+	FA_FL_CANCELED		= 2,
+};
+
+struct ldlm_flock_info {
+	struct file		*fa_file;
+	struct file_lock	*fa_fl; /* original file_lock */
+	struct file_lock	fa_flc; /* lock copy */
+	enum ldlm_flock_flags	fa_flags;
+	enum ldlm_mode		fa_mode;
+#ifdef HAVE_LM_GRANT_2ARGS
+	int (*fa_notify)(struct file_lock *, int);
+#else
+	int (*fa_notify)(struct file_lock *, struct file_lock *, int);
+#endif
+	int			fa_err;
+	int			fa_ready;
+	wait_queue_head_t       fa_waitq;
+};
+
 extern char *ldlm_lockname[];
 extern char *ldlm_typename[];
 extern const char *ldlm_it2str(enum ldlm_intent_flags it);
@@ -1421,6 +1447,9 @@ int ldlm_replay_locks(struct obd_import *imp);
 
 /* ldlm_flock.c */
 int ldlm_flock_completion_ast(struct ldlm_lock *lock, __u64 flags, void *data);
+struct ldlm_flock_info *
+ldlm_flock_completion_ast_async(struct ldlm_lock *lock, __u64 flags,
+				void *data);
 
 /* ldlm_extent.c */
 __u64 ldlm_extent_shift_kms(struct ldlm_lock *lock, __u64 old_kms);

lustre/include/obd.h
@@ -1244,6 +1244,10 @@ struct md_ops {
 			 const union ldlm_policy_data *, struct md_op_data *,
 			 struct lustre_handle *, __u64);
 
+	int (*m_enqueue_async)(struct obd_export *, struct ldlm_enqueue_info *,
+			       obd_enqueue_update_f, struct md_op_data *,
+			       const union ldlm_policy_data *, __u64);
+
 	int (*m_getattr)(struct obd_export *, struct md_op_data *,
 			 struct ptlrpc_request **);
 

lustre/include/obd_class.h
@@ -1393,6 +1393,7 @@ enum mps_stat_idx {
 	LPROC_MD_CLOSE,
 	LPROC_MD_CREATE,
 	LPROC_MD_ENQUEUE,
+	LPROC_MD_ENQUEUE_ASYNC,
 	LPROC_MD_GETATTR,
 	LPROC_MD_INTENT_LOCK,
 	LPROC_MD_LINK,
@@ -1506,6 +1507,29 @@ static inline int md_enqueue(struct obd_export *exp,
 	return exp->exp_obd->obd_type->typ_md_ops->m_enqueue(exp, einfo, policy,
 							     op_data, lockh,
 							     extra_lock_flags);
+}
+
+static inline int md_enqueue_async(struct obd_export *exp,
+				   struct ldlm_enqueue_info *einfo,
+				   obd_enqueue_update_f upcall,
+				   struct md_op_data *op_data,
+				   const union ldlm_policy_data *policy,
+				   __u64 lock_flags)
+{
+	int rc;
+
+	ENTRY;
+	rc = exp_check_ops(exp);
+	if (rc)
+		RETURN(rc);
+
+	lprocfs_counter_incr(exp->exp_obd->obd_md_stats,
+			     LPROC_MD_ENQUEUE_ASYNC);
+
+	rc = exp->exp_obd->obd_type->typ_md_ops->m_enqueue_async(exp, einfo,
+			upcall, op_data,
+			policy, lock_flags);
+	RETURN(rc);
 }
 
 static inline int md_getattr_name(struct obd_export *exp,

adpeacems/amlFilesystem-lustre

commit aa694b606f2c4509bc064bae56e69ef70b9d26d3

LU-18486 build: fl_flags removed in 6.8
Linux commit v6.7-rc4-79-gaf7628d6ec19
  fs: convert error_remove_page to error_remove_folio

Provide an abstraction for fl_flags and c.flc_flags

Make mdt_dom_fiemap a static function.

Test-Parameters: trivial
Fixes: 3177b0dc5d1 ("LU-2525 ldlm: add asynchronous flocks")
Fixes: 5921e1571f2 ("LU-14510 dom: fiemap support for DoM files")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Icb659a8ab19c00454391cbb7df31c2bb7f9a85c4

lustre/llite/file.c
@@ -5290,15 +5290,14 @@ static int ll_file_flock_lock(struct file *file, struct file_lock *file_lock)
 	 *    on the server.
 	 * 2. unlock - never conflicts with anything.
 	 */
-	file_lock->fl_flags &= ~FL_SLEEP;
+	file_lock->C_FLC_FLAGS &= ~FL_SLEEP;
 #ifdef HAVE_LOCKS_LOCK_FILE_WAIT
 	rc = locks_lock_file_wait(file, file_lock);
 #else
-	if (file_lock->fl_flags & FL_FLOCK) {
+	if (file_lock->C_FLC_FLAGS & FL_FLOCK)
 		rc = flock_lock_file_wait(file, file_lock);
-	} else if (file_lock->fl_flags & FL_POSIX) {
+	else if (file_lock->C_FLC_FLAGS & FL_POSIX)
 		rc = posix_lock_file(file, file_lock, NULL);
-	}
 #endif /* HAVE_LOCKS_LOCK_FILE_WAIT */
 	if (rc)
 		CDEBUG_LIMIT(rc == -ENOENT ? D_DLMTRACE : D_ERROR,
@@ -5571,7 +5570,7 @@ ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
 	    flags == LDLM_FL_BLOCK_NOWAIT /* F_SETLK/F_SETLK64 */) {
 
 		cb_data->fa_notify = file_lock->fl_lmops->lm_grant;
-		flags = (file_lock->fl_flags & FL_SLEEP) ?
+		flags = (file_lock->C_FLC_FLAGS & FL_SLEEP) ?
 			0 : LDLM_FL_BLOCK_NOWAIT;
 		einfo.ei_cb_cp = ll_flock_completion_ast_async;
 		get_file(file);
@@ -5592,7 +5591,7 @@ ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
 			 * with reordering of unlock & lock responses from
 			 * server.
 			 */
-			cb_data->fa_flc.fl_flags |= FL_EXISTS;
+			cb_data->fa_flc.C_FLC_FLAGS |= FL_EXISTS;
 			rc = ll_file_flock_lock(file, &cb_data->fa_flc);
 			if (rc) {
 				if (rc == -ENOENT) {

lustre/mdt/mdt_io.c
@@ -1096,8 +1096,8 @@ static int dom_has_zero_regions(struct fiemap *fiemap)
 	return begin < (fiemap->fm_start + fiemap->fm_length);
 }
 
-int mdt_dom_fiemap(const struct lu_env *env, struct mdt_device *mdt,
-		   const struct lu_fid *fid, struct fiemap *fiemap)
+static int mdt_dom_fiemap(const struct lu_env *env, struct mdt_device *mdt,
+			  const struct lu_fid *fid, struct fiemap *fiemap)
 {
 	struct mdt_object *mo;
 	int rc;